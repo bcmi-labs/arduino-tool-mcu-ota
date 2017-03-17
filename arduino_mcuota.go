f72b99e642f217bde37e51ee0ea5dfb5abc4b3c3
sebba <sebba@arduino.org>
2017-03-17 17:17:26 +0100

    Bug fixing
    Removing log lines; Minor bug fixing

--- arduino_mcuota.go
+++ arduino_mcuota.go
@@ -19,44 +19,34 @@ const endpoint = "otafile"
 
 
 func upload_files(list []string){
-    fmt.Println(" --- uploading file --- ")
-    url := "http://" + address + ":" + port + "/" + endpoint
+    url_base := "http://" + address + ":" + port + "/" + endpoint
     totchunk := len(list)
     
     fmt.Println("Sending " + filename + " to host " + address)
     
     for index, item := range list{        
     
-        url2 := url + "?totchunk=" + strconv.Itoa(totchunk) + "&numchunk=" + strconv.Itoa(index+1)
+        url := url_base + "?totchunk=" + strconv.Itoa(totchunk) + "&numchunk=" + strconv.Itoa(index+1)
         resp, err := resty.R().
         SetFile("files", item).
-        Post(url2)
-        
-        
-        fmt.Println(url2)
+        Post(url)
         
         if resp.StatusCode() == 200 || err != nil {
-            fmt.Println("["+strconv.Itoa(index+1)+"/"+strconv.Itoa(totchunk)+"] Done" + " | " + item)
-//            fmt.Println("Response Status Code: %v", resp.StatusCode())
-//            fmt.Println("Response Status: %v", resp.Status())
-//            fmt.Println("Response Time: %v", resp.Time())
-//            fmt.Println("Response Recevied At: %v", resp.ReceivedAt())
-//            fmt.Println("Response Body: %v", resp)
+            fmt.Println("["+strconv.Itoa(index+1)+" / "+strconv.Itoa(totchunk)+"] Done")
+            if index==totchunk {
+                fmt.Println("Upload all done")
+            }
         } else
         if resp.StatusCode() > 399 || err == nil {
-            fmt.Println("["+strconv.Itoa(index+1)+"/"+strconv.Itoa(totchunk)+"]")
-            fmt.Println("Error: %v", err)
-        } else {
-            fmt.Println("check")
+            fmt.Println("["+strconv.Itoa(index+1)+" / "+strconv.Itoa(totchunk)+"]")
+            fmt.Print("Error: ")
+            fmt.Println(err)
         }
-
     }
     clean(list)
 }
 
-
 func parse_args(){
-    fmt.Println(" --- getting args --- ")
     port = "80";
     nlines = "20";
     args := os.Args[1:]
@@ -73,19 +63,10 @@ func parse_args(){
                 address = args[i+1]
         }
     }
-    
-    
-    fmt.Println("FILE:  " + filename)
-    fmt.Println("LINES:  " + nlines)
-    fmt.Println("ADDRESS:  " + address)
-    fmt.Println("PORT:  " + port)
-
     split()
 }
 
-
 func split(){
-    fmt.Println(" --- splitting --- ")
     file_list := []string {}
 
     lines, err := strconv.Atoi(nlines)
@@ -104,13 +85,17 @@ func split(){
      var output string
         reader := bufio.NewReader(file)
         for {
-             line, _, err := reader.ReadLine()
+            line, _, err := reader.ReadLine()
 
-             if err == io.EOF {
-                 fmt.Println("ERROR : ")
-                 fmt.Println(err)
-                     break
-             }
+//            if err == nil {
+//                 fmt.Print("Error: ")
+//                 fmt.Println(err)
+//                 break
+//            }
+            
+            if err == io.EOF {
+                 break
+            }
 
           output += string(line) + "\n"
          
@@ -127,21 +112,16 @@ func split(){
                      counter ++   
             }
          index ++ 
-     }
-    
-    fmt.Println("FILES CREATED: " + strconv.Itoa(counter))
+     }    
     upload_files(file_list)
 }
 
-
 func clean(list []string){
-    fmt.Println(" --- deleting file --- ")
     counter:=1
     for _, item := range list{
         os.Remove(item)
         counter++
     }
-    fmt.Println("FILES DELETED: " + strconv.Itoa(counter))
 }
 
 func getFileLines(filename string)(int){
